622486edbbee701436f8c2e6ecb8df186e7613e0
chichoch <chichoch@gmail.com>
2015-11-16 13:13:45 +0100

    fixed gitignore

--- iptohtml.py
+++
@@ -1,9 +0,0 @@
-htmlFile = open('remotemulitview.html', 'w')
-ips = open('ipaddresses.txt', 'r').read().split()
-
-htmlFile.write('<html><body>')
-
-for ip in ips:
-	htmlFile.write('<iframe src="http://' + ip + ':63112/" width=500 height=300> </iframe>')
-
-htmlFile.write('</body></html>')
